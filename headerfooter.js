b2662ee6e497ba036be9128ebc67ea690425835f
sohelfitter07 <sohelfitter07@gmail.com>
2025-06-11 09:05:41 -0400

    Update headerfooter.js

--- headerfooter.js
+++ headerfooter.js
@@ -1,14 +1,14 @@
     // Simple JavaScript includes
-    document.addEventListener("DOMContentLoaded", function () {
-      fetch("header.html")
-        .then(res => res.text())
-        .then(data => {
-          document.getElementById("header").innerHTML = data;
-        });
+    // document.addEventListener("DOMContentLoaded", function () {
+    //   fetch("header.html")
+    //     .then(res => res.text())
+    //     .then(data => {
+    //       document.getElementById("header").innerHTML = data;
+    //     });
 
-      fetch("footer.html")
-        .then(res => res.text())
-        .then(data => {
-          document.getElementById("footer").innerHTML = data;
-        });
-    });
+    //   fetch("footer.html")
+    //     .then(res => res.text())
+    //     .then(data => {
+    //       document.getElementById("footer").innerHTML = data;
+    //     });
+    // });
