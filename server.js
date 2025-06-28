14b1ceb4bfcd3de41c2e7082878de7209181d9aa
sohelfitter07 <sohelfitter07@gmail.com>
2025-06-28 08:54:59 -0400

    Fix CORS to support both www and non-www domains

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3001;
 
 // Enable CORS for your domain
-app.use(cors({
-  origin: 'https://www.canadianfitnessrepair.com'
-}));
+const allowedOrigins = [
+  "https://canadianfitnessrepair.com",
+  "https://www.canadianfitnessrepair.com",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS: " + origin));
+      }
+    },
+  })
+);
 
 // Secure endpoint for Firebase config
-app.get('/api/firebase-config', (req, res) => {
+app.get("/api/firebase-config", (req, res) => {
   res.json({
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
@@ -18,7 +31,7 @@ app.get('/api/firebase-config', (req, res) => {
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
   });
 });
 
