e048a8c3176870cee77e76d0fc8cac4fb2750925
sohelfitter07 <sohelfitter07@gmail.com>
2025-06-27 20:27:43 -0400

    c

---
+++ api/firebase-config.js
@@ -0,0 +1,12 @@
+async function getFirebaseConfig() {
+    return {
+      apiKey: "AIzaSyCtz3qjz880jPQBH6jJ5fBY_O86wK1iRzs",
+      authDomain: "cfr-tracker.firebaseapp.com",
+      projectId: "cfr-tracker",
+      storageBucket: "cfr-tracker.appspot.com",
+      messagingSenderId: "870691427306",
+      appId: "1:870691427306:web:fc18ec2a33394e7fcf5893",
+      measurementId: "G-CMVSQHBQKH"
+    };
+  }
+  
